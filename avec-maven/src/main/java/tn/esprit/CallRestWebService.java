203dc0f8e65edda2d9ac40494940f48c6fba6e26
Oumayma-Bah <61434607+Oumayma-Bah@users.noreply.github.com>
2023-06-13 12:20:40 +0100

    sonarqube4

--- avec-maven/src/main/java/tn/esprit/CallRestWebService.java
+++ avec-maven/src/main/java/tn/esprit/CallRestWebService.java
@@ -2,40 +2,38 @@ package tn.esprit;
 
 import java.io.IOException;
 
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
+import org.json.JSONException;
 import org.json.JSONObject;
-
-/**
- * @author Walid-YAICH
- * URL utiles :
- * http://theoryapp.com/parse-json-in-java/
- * https://jsonformatter.curiousconcept.com/
- */
 
 public class CallRestWebService {
 
-	
 	public static final String endpoint = "http://ip-api.com/json";
+
 	public static void main(String[] args) {
-		HttpClient client = new DefaultHttpClient();
+		HttpClient client = HttpClients.createDefault();
 		HttpGet request = new HttpGet(endpoint);
 		String ip = "not found";
+
 		try {
 			HttpResponse response = client.execute(request);
-			String jsonResponse = EntityUtils.toString(response.getEntity());
+			HttpEntity entity = response.getEntity();
+			String jsonResponse = EntityUtils.toString(entity);
 			System.out.println("Response as String : " + jsonResponse);
-			JSONObject responseObj = new JSONObject(jsonResponse);
 
+			JSONObject responseObj = new JSONObject(jsonResponse);
 			ip = responseObj.getString("query");
 			System.out.println("ip : " + ip);
 
 		} catch (IOException e) {
 			e.printStackTrace();
+		} catch (JSONException e) {
+			e.printStackTrace();
 		}
 	}
 }
- 
